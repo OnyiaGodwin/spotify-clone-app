9ca8f0b23b2747617742e5a96640343f7f246a0a
GodwinOnyia <godwinonyiagodwin@gmail.com>
2022-07-05 14:00:13 +0100

    changes on the Footer.js file to fix import error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import RepeatIcon from "@mui/icons-material/Repeat";
-import PlaylistPlayIcon from "@mui/icons-material";
-import VolumeDownIcon from "@mui/icons-material";
-import { Grid, Slider } from "@mui/material";
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+//import { Grid, Slider } from "@mui/material";
 
 import classes from "./Footer.module.css";
 //import { useDataLayerValue } from "../use-Context/DataLayer";
@@ -18,9 +20,9 @@ function Footer() {
   return (
     <div className={classes.footer}>
       <div className={classes.footer__left}>
-        <img src="" alt="image" />
+        <img src="" alt="" />
         <div className={classes.footer__songInfo}>
-          <h4></h4>
+          <h4>Playing song</h4>
           <p></p>
         </div>
       </div>
@@ -37,7 +39,7 @@ function Footer() {
       </div>
 
       <div className={classes.footer__right}>
-        <Grid container spacing={2}>
+        {<Grid container spacing={2}>
           <Grid item>
             <PlaylistPlayIcon />
           </Grid>
@@ -45,9 +47,9 @@ function Footer() {
             <VolumeDownIcon />
           </Grid>
           <Grid item xs>
-            <Slider />
+          <Slider />
           </Grid>
-        </Grid>
+        </Grid>}
       </div>
     </div>
   );
