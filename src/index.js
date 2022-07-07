ca97cb0a4050a483a6b37a0f61ea3d2d8f588346
GodwinOnyia <godwinonyiagodwin@gmail.com>
2022-07-07 19:27:11 +0100

    Issue of nothing being displayed solved by adding return in DataLayer.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import { DataLayer } from "./use-Context/DataLayer";
 import reducer, { initialState } from "./use-Context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <DataLayer initialState={initialState} reducer={reducer}>
+root.render(<DataLayer initialState={initialState} reducer={reducer}>
       <App />
     </DataLayer>
 );
